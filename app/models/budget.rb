d1cc37721455ba493c9465af723b941478a191a3
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-13 20:31:21 +0100

    Add validation and finish relationship in models

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,6 @@
 class Budget < ApplicationRecord
   belongs_to :author
+
+  validates :name, presence: true
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :author
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
