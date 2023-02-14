9a3dff38fdcb26684ef8d0c861a76f76aa435c5b
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-14 16:12:10 +0100

    Add the feature to add budget

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,5 +1,17 @@
 class BudgetsController < ApplicationController
-  def new; end
+  def new
+    @budget = Budget.new
+    @categories = Group.where(author_id: current_user.id)
+  end
+
+  def create
+    new_budget = Budget.new(name: params[:budget][:name], amount: params[:budget][:amount], group_id: params[:budget][:group_id], author: current_user)
+    if new_budget.save
+      redirect_to budgets_path
+    else
+      render :new
+    end
+  end
 
   def index; end
 end
