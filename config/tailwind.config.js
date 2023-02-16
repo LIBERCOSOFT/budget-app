5ccde5bc93c1aecd38b2b9e3a4c76625ecad15dc
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-02-16 11:53:57 +0100

    Add styles to the root page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,26 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        proxima_bold: ['proxima_nova_rgbold', ...defaultTheme.fontFamily.sans],
+        proxima_regular: [
+          'proxima_nova_rgregular',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      colors: {
+        main: '#3778c2',
+        secondary: '#5fb523',
+        text: '#434b54',
       },
     },
   },
@@ -18,5 +28,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
-}
+  ],
+};
